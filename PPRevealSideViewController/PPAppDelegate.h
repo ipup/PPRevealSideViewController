7fb744592b3f224fef7eb16fe50bef81783b6d0f
ipodishima <marian.paul2@gmail.com>
2012-02-16 13:13:21 +0100

    Initial Commit

---
+++ PPRevealSideViewController/PPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PPAppDelegate.h
+//  PPRevealSideViewController
+//
+//  Created by Marian PAUL on 16/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PPRevealSideViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PPRevealSideViewController
+//
+//  Created by Marian PAUL on 16/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PPAppDelegate class]));
+    }
+}
