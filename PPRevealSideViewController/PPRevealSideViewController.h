15d7707164983f17c9bc25d2944c143ccc4e45e9
ipodishima <marian.paul2@gmail.com>
2012-02-16 13:15:11 +0100

    initial import of main controller

---
+++ PPRevealSideViewController/PPRevealSideViewController.h
@@ -0,0 +1,13 @@
+//
+//  PPRevealSideViewController.h
+//  PPRevealSideViewController
+//
+//  Created by Marian PAUL on 16/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPRevealSideViewController : UIViewController
+
+@end
---
+++ PPRevealSideViewController/PPRevealSideViewController.m
@@ -0,0 +1,60 @@
+//
+//  PPRevealSideViewController.m
+//  PPRevealSideViewController
+//
+//  Created by Marian PAUL on 16/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PPRevealSideViewController.h"
+
+@implementation PPRevealSideViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+*/
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+*/
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
