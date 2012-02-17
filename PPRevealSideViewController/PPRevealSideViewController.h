40caab8e1b525dc1eb507d9c00262c09b9af501b
ipodishima <marian.paul2@gmail.com>
2012-02-17 09:37:33 +0100

    commit before morning work =)

--- PPRevealSideViewController/PPRevealSideViewController.h
+++ PPRevealSideViewController/PPRevealSideViewController.h
@@ -8,6 +8,114 @@
 
 #import <UIKit/UIKit.h>
 
+// define some macros 
+#ifndef __has_feature
+#define __has_feature(x) 0
+#endif
+#ifndef __has_extension
+#define __has_extension __has_feature // Compatibility with pre-3.0 compilers.
+#endif
+
+#if __has_feature(objc_arc) && __clang_major__ >= 3
+#define PP_ARC_ENABLED 1
+#endif // __has_feature(objc_arc)
+
+#if PP_ARC_ENABLED
+#define PP_RETAIN(xx) (xx)
+#define PP_RELEASE(xx)  xx = nil
+#define PP_AUTORELEASE(xx)  (xx)
+#else
+#define PP_RETAIN(xx)           [xx retain]
+#define PP_RELEASE(xx)          [xx release], xx = nil
+#define PP_AUTORELEASE(xx)      [xx autorelease]
+#endif
+
+#ifndef PPLog
+# define PPLog(fmt, ...) NSLog((@"%s [Line %d] " fmt),__PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+//#define PPLog(fmt, ...)
+#endif
+
+typedef enum {
+    PPRevealSideDirectionLeft = 0,
+    PPRevealSideDirectionRight,
+    PPRevealSideDirectionTop,
+    PPRevealSideDirectionBottom,
+    PPRevealSideDirectionNone = -1 // this cannot be used
+} PPRevealSideDirection;
+
+enum {
+    PPRevealSideInteractionNone = 0,
+    PPRevealSideInteractionNavigationBar = 1 << 1,
+    PPRevealSideInteractionContentView = 1 << 2,
+    
+};
+typedef NSUInteger PPRevealSideInteraction;
+
+@protocol PPRevealSideViewControllerDelegate;
+
 @interface PPRevealSideViewController : UIViewController
+{
+    NSMutableDictionary *_viewControllers;
+    NSMutableDictionary *_viewControllersOffsets;
+}
+/**
+ Initialize the reveal controller with a rootViewController. This rootViewController will be in the center
+ @param rootViewController the center view controller
+ */
+- (id) initWithRootViewController:(UIViewController*)rootViewController;
+
+
+/**
+ Getter for the rootViewController
+ */
+@property (nonatomic, readonly, retain) UIViewController *rootViewController;
+
+/**
+ Disable or enable the shadows. Enabled by default
+ */
+@property (nonatomic, assign) BOOL showShadows;
+
+/**
+ Decide if the animations are boucing or not. By default, they are
+ */
+@property (nonatomic, assign) BOOL bouncingAnimations;
+
+/**
+ The offset bouncing. 
+ When opening, if set to -1.0, the the animation will bounce with a default offset
+ When closing, if set to -1.0, then the animation open completely before closing. 
+ Set to -1.0 by default
+ */
+@property (nonatomic, assign) CGFloat bouncingOffset;
+
+/**
+ Define the interactions to display the side views. By default, only the navigation bar is enabled
+ */
+@property (nonatomic, assign) PPRevealSideDirection interactions;
+
+@property (nonatomic, assign) id <PPRevealSideViewControllerDelegate> delegate;
+
+- (void) pushViewController:(UIViewController*)controller onDirection:(PPRevealSideDirection)direction animated:(BOOL)animated;
+- (void) pushViewController:(UIViewController*)controller onDirection:(PPRevealSideDirection)direction withOffset:(CGFloat)offset animated:(BOOL)animated;
+- (void) popViewControllerWithNewCenterController:(UIViewController*)centerController animated:(BOOL)animated;
+
+- (void) preloadViewController:(UIViewController*)controller forSide:(PPRevealSideDirection)direction;
 
 @end
+
+
+@interface UIViewController (PPRevealSideViewController)
+@property (nonatomic, retain) PPRevealSideViewController *revealSideViewController;
+@end
+
+@protocol PPRevealSideViewControllerDelegate <NSObject>
+@optional
+- (void) pprevealSideViewController:(PPRevealSideViewController*)controller didChangeCenterController:(UIViewController*)newCenterController;
+- (void) pprevealSideViewController:(PPRevealSideViewController *)controller willPushController:(UIViewController *)pushedController;
+- (void) pprevealSideViewController:(PPRevealSideViewController *)controller didPushController:(UIViewController *)pushedController;
+- (void) pprevealSideViewController:(PPRevealSideViewController *)controller willPopToController:(UIViewController *)centerController;
+- (void) pprevealSideViewController:(PPRevealSideViewController *)controller didPopToController:(UIViewController *)centerController;
+@end
+
+UIInterfaceOrientation PPInterfaceOrientation();
+CGRect PPScreenBounds();
--- PPRevealSideViewController/PPRevealSideViewController.m
+++ PPRevealSideViewController/PPRevealSideViewController.m
@@ -7,34 +7,59 @@
 //
 
 #import "PPRevealSideViewController.h"
+#import <QuartzCore/QuartzCore.h>
+#import <objc/runtime.h>
+
+@interface PPRevealSideViewController (Private)
+- (void) setRootViewController:(UIViewController*)controller;
+- (void) handleShadows;
+- (void) informDelegateWithOptionalSelector:(SEL)selector withParam:(id)param;
+
+- (BOOL) isLeftControllerClosed;
+- (BOOL) isRightControllerClosed;
+- (BOOL) isTopControllerClosed;
+- (BOOL) isBottomControllerClosed;
+
+- (PPRevealSideDirection) getSideToClose;
+
+- (CGRect) getSlidingRectForOffset:(CGFloat)offset forDirection:(PPRevealSideDirection)direction;
+
+@end
 
 @implementation PPRevealSideViewController
+@synthesize rootViewController = _rootViewController;
+@synthesize showShadows = _showShadows;
+@synthesize interactions = _interactions;
+@synthesize bouncingAnimations = _bouncingAnimations;
+@synthesize bouncingOffset = _bouncingOffset;
+@synthesize delegate = _delegate;
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+- (id) initWithRootViewController:(UIViewController*)rootViewController {
+    self = [super init];
     if (self) {
-        // Custom initialization
+        [self setRootViewController:rootViewController];
+        self.showShadows = YES;
+        self.bouncingAnimations = YES;
+        self.bouncingOffset = -1.0;
+        self.interactions = PPRevealSideInteractionNavigationBar;
+        _viewControllers = [[NSMutableDictionary alloc] init];
+        _viewControllersOffsets = [[NSMutableDictionary alloc] init];
+
     }
     return self;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
-}
-
 #pragma mark - View lifecycle
 
-/*
 // Implement loadView to create a view hierarchy programmatically, without using a nib.
 - (void)loadView
 {
+    self.view = PP_AUTORELEASE([[UIView alloc] initWithFrame:PPScreenBounds()]);
+    self.view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    self.view.clipsToBounds = YES;
+    self.view.autoresizesSubviews = YES;
 }
-*/
+
 
 /*
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
@@ -44,6 +69,249 @@
 }
 */
 
+#pragma mark - Push and pop methods
+#define DefaultOffset 50.0
+#define DefaultOffsetBouncing 5.0
+
+#define OpenAnimationTime 0.5
+#define OpenAnimationTimeBouncingRatio 0.3
+
+- (void) pushViewController:(UIViewController*)controller onDirection:(PPRevealSideDirection)direction animated:(BOOL)animated {
+    [self pushViewController:controller
+                 onDirection:direction
+                  withOffset:DefaultOffset
+                    animated:animated];
+}
+
+- (void) pushViewController:(UIViewController*)controller onDirection:(PPRevealSideDirection)direction withOffset:(CGFloat)offset animated:(BOOL)animated {
+    [self informDelegateWithOptionalSelector:@selector(pprevealSideViewController:willPushController:) withParam:controller];
+    
+    // save the offset
+    [_viewControllersOffsets setObject:[NSNumber numberWithFloat:offset] forKey:[NSNumber numberWithInt:direction]];
+    
+    // save the controller
+    [_viewControllers setObject:controller forKey:[NSNumber numberWithInt:direction]];
+    
+    // get the side direction to close
+    PPRevealSideDirection directionToClose = [self getSideToClose];
+    
+    if (controller.view.superview != self.view) [controller.view removeFromSuperview], [self.view insertSubview:controller.view belowSubview:_rootViewController.view];
+    
+    // replace with the bounds since IB add some offsets with the status bar if enabled
+    controller.view.frame = controller.view.bounds;
+    
+    void (^closeAnimBlock)(void) = ^(void) {
+        controller.view.hidden = NO;
+        
+        if (_bouncingAnimations && animated) // then we make an offset
+            _rootViewController.view.frame = [self getSlidingRectForOffset:offset- ((_bouncingOffset == - 1.0) ? DefaultOffsetBouncing : _bouncingOffset) forDirection:direction];
+        else
+            _rootViewController.view.frame = [self getSlidingRectForOffset:offset forDirection:direction];
+        
+    };
+    
+    NSTimeInterval animationTime;
+    if (_bouncingAnimations) animationTime = OpenAnimationTime*(1.0-OpenAnimationTimeBouncingRatio);
+    else animationTime = OpenAnimationTime;
+    
+    UIViewAnimationOptions options = UIViewAnimationOptionCurveEaseInOut | UIViewAnimationOptionLayoutSubviews;
+    
+    if (animated) {
+        [UIView animateWithDuration:animationTime
+                              delay:0.0
+                            options:options
+                         animations:closeAnimBlock
+                         completion:^(BOOL finished) {
+                             if (_bouncingAnimations) // then we come to normal
+                             {
+                                 [UIView animateWithDuration:OpenAnimationTime*OpenAnimationTimeBouncingRatio
+                                                       delay:0.0
+                                                     options:options
+                                                  animations:^{
+                                                      _rootViewController.view.frame = [self getSlidingRectForOffset:offset forDirection:direction];
+                                                  } completion:^(BOOL finished) {
+                                                      [self informDelegateWithOptionalSelector:@selector(pprevealSideViewController:didPushController:) withParam:controller];
+                                                  }];
+                             }
+                             else
+                                 [self informDelegateWithOptionalSelector:@selector(pprevealSideViewController:didPushController:) withParam:controller];
+                                 
+                         }];
+    }
+    else
+        closeAnimBlock();
+}
+
+- (void) popViewControllerWithNewCenterController:(UIViewController*)centerController animated:(BOOL)animated {
+    [self informDelegateWithOptionalSelector:@selector(pprevealSideViewController:willPopToController:) withParam:centerController];
+
+}
+
+- (void) preloadViewController:(UIViewController*)controller forSide:(PPRevealSideDirection)direction {
+    UIViewController *existingController = [_viewControllers objectForKey:[NSNumber numberWithInt:direction]];
+    if (existingController != controller) {
+        
+        if (existingController.view.superview) [existingController.view removeFromSuperview];
+        
+        [_viewControllers setObject:controller forKey:[NSNumber numberWithInt:direction]];
+        if (![controller isViewLoaded]) {
+            [self.view insertSubview:controller.view atIndex:0];
+            controller.view.hidden = YES;
+        }
+        
+    }
+}
+
+#pragma mark - Setters
+
+- (void) setShowShadows:(BOOL)showShadows {
+    [self willChangeValueForKey:@"showShadows"];
+    _showShadows = showShadows;
+    [self handleShadows];
+    [self didChangeValueForKey:@"showShadows"];
+}
+
+#pragma mark - Private methods
+
+- (void) setRootViewController:(UIViewController *)controller {
+    if (_rootViewController != controller) {
+        [self willChangeValueForKey:@"rootViewController"];
+        [_rootViewController.view removeFromSuperview];
+        
+        _rootViewController = PP_RETAIN(controller);
+        _rootViewController.revealSideViewController = self;
+        
+        [self handleShadows];
+        
+        [self.view addSubview:_rootViewController.view];
+        [self didChangeValueForKey:@"rootViewController"];
+    }
+}
+
+- (void) handleShadows {
+    if (self.showShadows) {
+        _rootViewController.view.layer.shadowOffset = CGSizeZero;
+        _rootViewController.view.layer.shadowOpacity = 0.75f;
+        _rootViewController.view.layer.shadowRadius = 10.0f;
+        _rootViewController.view.layer.shadowColor = [UIColor blackColor].CGColor;
+        _rootViewController.view.layer.shadowPath = [UIBezierPath bezierPathWithRect:self.view.layer.bounds].CGPath;
+        _rootViewController.view.clipsToBounds = NO; 
+    }
+    else {
+        _rootViewController.view.layer.shadowPath = nil;
+        _rootViewController.view.layer.shadowOpacity = 0.0f;
+        _rootViewController.view.layer.shadowRadius = 0.0;
+        _rootViewController.view.layer.shadowColor = nil;
+    }
+}
+
+- (void) informDelegateWithOptionalSelector:(SEL)selector withParam:(id)param {
+    if ([self.delegate respondsToSelector:selector]) {
+        // suppression of 'performSelector may cause a leak because its selector is unknown' warning
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+        [self.delegate performSelector:selector withObject:self withObject:param];
+#pragma clang diagnostic pop
+    }
+}
+
+#pragma mark Closed Controllers 
+
+- (BOOL) isLeftControllerClosed {
+    UIViewController *c = [_viewControllers objectForKey:[NSNumber numberWithInt:PPRevealSideDirectionLeft]];
+    return !c && CGRectGetMinX(_rootViewController.view.frame) <= 0;
+}
+
+- (BOOL) isRightControllerClosed {
+    UIViewController *c = [_viewControllers objectForKey:[NSNumber numberWithInt:PPRevealSideDirectionRight]];
+    return !c && CGRectGetMaxX(_rootViewController.view.frame) <= CGRectGetWidth(_rootViewController.view.frame);
+}
+
+- (BOOL) isTopControllerClosed {
+    UIViewController *c = [_viewControllers objectForKey:[NSNumber numberWithInt:PPRevealSideDirectionTop]];
+    return !c && CGRectGetMinY(_rootViewController.view.frame) <= 0;
+}
+
+- (BOOL) isBottomControllerClosed {
+    UIViewController *c = [_viewControllers objectForKey:[NSNumber numberWithInt:PPRevealSideDirectionBottom]];
+    return !c && CGRectGetMaxY(_rootViewController.view.frame) <= CGRectGetHeight(_rootViewController.view.frame); 
+}
+
+- (PPRevealSideDirection) getSideToClose {
+    PPRevealSideDirection sideToReturn = PPRevealSideDirectionNone;
+    if (![self isRightControllerClosed]) sideToReturn = PPRevealSideDirectionRight;
+    if (![self isLeftControllerClosed]) sideToReturn = PPRevealSideDirectionLeft;
+    if (![self isTopControllerClosed]) sideToReturn = PPRevealSideDirectionTop;
+    if (![self isBottomControllerClosed]) sideToReturn = PPRevealSideDirectionBottom;
+    return sideToReturn;
+}
+
+- (CGRect) getSlidingRectForOffset:(CGFloat)offset forDirection:(PPRevealSideDirection)direction {
+    if (direction == PPRevealSideDirectionLeft || direction == PPRevealSideDirectionRight) offset = MIN(CGRectGetWidth(PPScreenBounds()), offset);
+    
+    if (direction == PPRevealSideDirectionTop || direction == PPRevealSideDirectionBottom) offset = MIN(CGRectGetHeight(PPScreenBounds()), offset);
+
+    CGRect rectToReturn = CGRectZero;
+    rectToReturn.size = _rootViewController.view.frame.size;
+    
+    switch (direction) {
+        case PPRevealSideDirectionLeft:
+            rectToReturn.origin = CGPointMake(CGRectGetWidth(_rootViewController.view.frame)-offset, 0.0);
+            break;
+        case PPRevealSideDirectionRight:
+            rectToReturn.origin = CGPointMake(-offset, 0.0);
+            break;
+        case PPRevealSideDirectionBottom:
+            rectToReturn.origin = CGPointMake(0.0, -offset);
+            break;
+        case PPRevealSideDirectionTop:
+            rectToReturn.origin = CGPointMake(0.0, CGRectGetHeight(_rootViewController.view.frame)-offset);
+            break;   
+        default:
+            break;
+    }
+    return rectToReturn;
+}
+
+#pragma mark - Orientation stuff
+
+- (void) willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+    [super willAnimateRotationToInterfaceOrientation:toInterfaceOrientation duration:duration];
+    
+    _rootViewController.view.layer.shadowPath = nil;
+    _rootViewController.view.layer.shouldRasterize = YES;
+    
+    [_rootViewController willAnimateRotationToInterfaceOrientation:toInterfaceOrientation duration:duration];
+    
+    for (id key in _viewControllers.allKeys)
+        [(UIViewController *)[_viewControllers objectForKey:key] willAnimateRotationToInterfaceOrientation:toInterfaceOrientation duration:duration];
+}
+
+- (void) didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
+    [super didRotateFromInterfaceOrientation:fromInterfaceOrientation];
+    
+    _rootViewController.view.layer.shadowPath = [UIBezierPath bezierPathWithRect:_rootViewController.view.layer.bounds].CGPath;
+    _rootViewController.view.layer.shouldRasterize = NO;
+    
+    [_rootViewController didRotateFromInterfaceOrientation:fromInterfaceOrientation];
+    for (id key in _viewControllers.allKeys)
+        [(UIViewController *)[_viewControllers objectForKey:key] didRotateFromInterfaceOrientation:fromInterfaceOrientation];
+}
+
+- (BOOL) shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
+    return [_rootViewController shouldAutorotateToInterfaceOrientation:toInterfaceOrientation];
+}
+
+#pragma mark - Memory management things
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
 - (void)viewDidUnload
 {
     [super viewDidUnload];
@@ -51,10 +319,55 @@
     // e.g. self.myOutlet = nil;
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+- (void) dealloc {
+    PP_RELEASE(_rootViewController);
+    PP_RELEASE(_viewControllers);
+    PP_RELEASE(_viewControllersOffsets);
+
+#if !PP_ARC_ENABLED
+ [super dealloc];
+#endif
 }
 
 @end
+
+
+
+
+@implementation UIViewController (PPRevealSideViewController)
+static char revealSideViewControllerKey;
+
+- (void) setRevealSideViewController:(PPRevealSideViewController *)revealSideViewController {
+    objc_setAssociatedObject( self, 
+                             &revealSideViewControllerKey,
+                             revealSideViewController,
+                             OBJC_ASSOCIATION_RETAIN );
+}
+
+- (PPRevealSideViewController*) revealSideViewController {
+    id controller = objc_getAssociatedObject( self, 
+                                             &revealSideViewControllerKey );
+    
+    // because we can't ask the navigation controller to set to the pushed controller the revealSideViewController !
+    if (!controller && self.navigationController)
+        controller = self.navigationController.revealSideViewController;
+
+    return controller;
+}
+
+@end
+
+UIInterfaceOrientation PPInterfaceOrientation() {
+	UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+	return orientation;
+}
+
+CGRect PPScreenBounds() {
+	CGRect bounds = [UIScreen mainScreen].bounds;
+	if (UIInterfaceOrientationIsLandscape(PPInterfaceOrientation())) {
+		CGFloat width = bounds.size.width;
+		bounds.size.width = bounds.size.height;
+		bounds.size.height = width;
+	}
+	return bounds;
+}
